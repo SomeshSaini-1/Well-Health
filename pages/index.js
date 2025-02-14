e0b81ebfdaf0d2b9b0a11d2c4116e940613b601a
Somesh saini <158258175+SomeshSaini-1@users.noreply.github.com>
2025-02-14 10:57:37 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,7 @@ export default function Start() {
     <div>
       <Head>
         <title>Health & Wellness</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
       {timer ? <Philosophy /> :
